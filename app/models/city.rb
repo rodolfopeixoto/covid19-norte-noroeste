95c82d77639e0985f1fc29aef81807ca7640ab8f
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2020-09-22 14:01:52 -0300

    Add positive active

--- app/models/city.rb
+++ app/models/city.rb
@@ -17,4 +17,12 @@ class City < ApplicationRecord
     order('covid_informations.confirmed DESC').
     pluck("cities.name, covid_informations.confirmed")
   end
+
+  def self.max_positive_active_by_city
+    CovidInformation.includes(:city).
+    where(covid_informations: { date_reference: CovidInformation.maximum(:date_reference)  } ).
+    group(["cities.name","covid_informations.positive_active"]).
+    order('covid_informations.positive_active DESC').
+    pluck("cities.name, covid_informations.positive_active")
+  end
 end
